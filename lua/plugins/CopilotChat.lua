2c833381b02d40d1465cb05a6bd7e2fd9b0c5686
AeonJh Fu <aeonjh@nanoleaf.me>
2024-04-15 18:02:37 +0800

    fix: Fixed conflicts caused by upgrading CopilotChat.nvim

--- lua/plugins/CopilotChat.lua
+++ lua/plugins/CopilotChat.lua
@@ -32,19 +32,78 @@ return {
     },
 
     opts = {
-      debug = false, -- Disable debugging
-      prompts = prompts,
-      auto_follow_cursor = false, -- Don't follow the cursor after getting response
-      show_help = true, -- Show help in virtual text
+      debug = false, -- Enable debug logging
+      proxy = nil, -- [protocol://]host[:port] Use this proxy
+      allow_insecure = false, -- Allow insecure server connections
+
+      prompts = prompts, -- System prompt to use
+      model = 'gpt-4', -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
+      temperature = 0.1, -- GPT temperature
+
+      question_header = '## User ', -- Header to use for user questions
+      answer_header = '## Copilot ', -- Header to use for AI answers
+      error_header = '## Error ', -- Header to use for errors
+      separator = '---', -- Separator to use in chat
+
+      show_folds = true, -- Shows folds for sections in chat
+      show_help = true, -- Shows help message as virtual lines when waiting for user input
+      auto_follow_cursor = true, -- Auto-follow cursor in chat
+      auto_insert_mode = false, -- Automatically enter insert mode when opening window and if auto follow cursor is enabled on new prompt
+      clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
+
+      context = nil, -- Default context to use, 'buffers', 'buffer' or none (can be specified manually in prompt via @).
+      history_path = vim.fn.stdpath('data') .. '/copilotchat_history', -- Default path to stored history
+      callback = nil, -- Callback to use when ask response is received
+
+      -- default window options
+      window = {
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+        -- Options below only apply to floating windows
+        relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
+        border = 'single', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        width = 0.8, -- fractional width of parent
+        height = 0.6, -- fractional height of parent
+        row = nil, -- row position of the window, default is centered
+        col = nil, -- column position of the window, default is centered
+        title = 'Copilot Chat', -- title of chat window
+        footer = nil, -- footer of chat window
+        zindex = 1, -- determines if window is on top or below other floating windows
+      },
+
+      -- default mappings
       mappings = {
-        close = "q", -- Close chat
-        reset = "<C-l>", -- Clear the chat buffer
-        complete = "<Tab>", -- Change to insert mode and press tab to get the completion
-        submit_prompt = "<CR>", -- Submit question to Copilot Chat
-        accept_diff = "<C-a>", -- Accept the diff
-        show_diff = "<C-s>", -- Show the diff
-        show_system_prompt = "gmp", -- Show system prompt
-        show_user_selection = "gms", -- Show user selection
+        complete = {
+          detail = 'Use @<Tab> or /<Tab> for options.',
+          insert ='<Tab>',
+        },
+        close = {
+          normal = 'q',
+          insert = '<C-c>'
+        },
+        reset = {
+          normal ='<C-l>',
+          insert = '<C-l>'
+        },
+        submit_prompt = {
+          normal = '<CR>',
+          insert = '<C-m>'
+        },
+        accept_diff = {
+          normal = '<C-y>',
+          insert = '<C-y>'
+        },
+        yank_diff = {
+          normal = 'gy',
+        },
+        show_diff = {
+          normal = 'gd'
+        },
+        show_system_prompt = {
+          normal = 'gp'
+        },
+        show_user_selection = {
+          normal = 'gs'
+        },
       },
     },
 
