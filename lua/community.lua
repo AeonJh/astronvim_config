688b698c27e0b330e718b6437833fa2bd07f8dd4
AeonJh Fu <aeonjh@nanoleaf.me>
2025-05-21 16:54:58 +0800

    fix: fix wrong state of CopilotChat.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -13,5 +13,4 @@ return {
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.color.transparent-nvim" },
-  { import = "astrocommunity.editing-support.copilotchat-nvim" }
 }
